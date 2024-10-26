0f24058efe160969e89662c01a3e1d31bc7bdc35
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2024-10-25 22:05:03 -0300

    Fix issues with JEI Mojmap shenanigans

--- src/main/java/io/github/ennuil/ennuis_bigger_gambiarra/mixin/jei/BasicRecipeTransferInfoMixin.java
+++ src/main/java/io/github/ennuil/ennuis_bigger_gambiarra/mixin/jei/BasicRecipeTransferInfoMixin.java
@@ -9,9 +9,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BasicRecipeTransferInfo.class)
-public abstract class PlayerRecipeTransferHandlerMixin {
-	@ModifyExpressionValue(method = "getInventorySlots", at = @At(value = "FIELD", target = "Lmezz/jei/library/transfer/BasicRecipeTransferInfo;inventorySlotCount:I"))
-	private int modifySlotEnd(int original, @Local ScreenHandler screenHandler) {
+public abstract class BasicRecipeTransferInfoMixin {
+	@ModifyExpressionValue(
+		method = "getInventorySlots",
+		at = @At(
+			value = "FIELD",
+			target = "Lmezz/jei/library/transfer/BasicRecipeTransferInfo;inventorySlotCount:I",
+			remap = false
+		),
+		remap = false
+	)
+	private int modifySlotEnd(int original, @Local(index = 0) ScreenHandler screenHandler) {
 		// This hackjob is generic enough to affect everything that's needed but it might be overreaching!
 		// I recommend EMI for a better experience, but if you really want JEI, I guess we have this ticking time bomb
 		// Do *not* point fingers to JEI's mod authors!!! I'm the one doing the gambiarra in the first place!
